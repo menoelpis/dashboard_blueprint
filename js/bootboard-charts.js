74990fb6daf1fae88e676c0bcc2c1d0f31e59138
Daniel Park <menoelpis@gmail.com>
2016-04-17 12:26:00 +0900

    Finished Dashboard

--- js/bootboard-charts.js
+++ js/bootboard-charts.js
@@ -42,3 +42,17 @@ new Chartist.Pie('.ct-chart-three', data, {
         return Math.round(value / data.series.reduce(sum) * 100) + '%';
     }
 });
+
+new Chartist.Line('.ct-chart-table', {
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+    series: [
+        [12, 9, 7, 8, 5],
+        [2, 1, 3.5, 7, 3],
+        [1, 3, 4, 5, 6]
+    ]
+}, {
+    fullWidth: true,
+    chartPadding: {
+        right: 40
+    }
+});
